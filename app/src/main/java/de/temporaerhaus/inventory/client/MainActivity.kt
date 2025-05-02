98c4275a334d0dd6484257f7da6e16f14912ec52
robbi5 <maxi@richt.name>
2025-05-02 14:40:13 +0200

    Add intent support for zebra devices, configuring datawedge using a config intent

--- app/src/main/java/de/temporaerhaus/inventory/client/MainActivity.kt
+++ app/src/main/java/de/temporaerhaus/inventory/client/MainActivity.kt
@@ -1,7 +1,10 @@
 package de.temporaerhaus.inventory.client
 
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -41,6 +44,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -85,16 +89,95 @@ import java.time.format.DateTimeParseException
 
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        const val SCAN_INTENT = "de.temporaerhaus.inventory.client.SCAN"
+    }
+
+    private val _barcodeState = mutableStateOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TPHInventoryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    InventoryApp(modifier = Modifier.padding(innerPadding))
+                    InventoryApp(
+                        modifier = Modifier.padding(innerPadding),
+                        barcodeBroadcastState = _barcodeState,
+                    )
                 }
             }
         }
+
+        // Check if the app is running on a device with installed zebra datawedge:
+        val isDataWedgeInstalled = try {
+            packageManager.getPackageInfo("com.symbol.datawedge", 0)
+            true
+        } catch (_: PackageManager.NameNotFoundException) {
+            false
+        }
+        if (isDataWedgeInstalled) {
+            configureDataWedge()
+        } else {
+            Log.d("MainActivity", "Not a Zebra device, not enabling data wedge.")
+        }
+    }
+
+    private val barcodeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == SCAN_INTENT) {
+                val barcode = intent.getStringExtra("com.symbol.datawedge.data_string") ?: ""
+                Log.d("MainActivity", "Received barcode: $barcode")
+                _barcodeState.value = barcode
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val intentFilter = IntentFilter(SCAN_INTENT).apply {
+            addCategory(Intent.CATEGORY_DEFAULT)
+        }
+        ContextCompat.registerReceiver(this, barcodeReceiver, intentFilter, ContextCompat.RECEIVER_EXPORTED)
+    }
+
+    override fun onPause() {
+        unregisterReceiver(barcodeReceiver)
+        super.onPause()
+    }
+
+    fun configureDataWedge() {
+        val USE_BROADCAST = 2
+
+        val config = Bundle().apply {
+            putString("PROFILE_NAME", "tph-inventory-client")
+            putString("PROFILE_ENABLED", "true")
+            putString("CONFIG_MODE", "CREATE_IF_NOT_EXIST")
+            putParcelableArray("APP_LIST", arrayOf<Bundle?>(
+                    Bundle().apply {
+                        putString("PACKAGE_NAME", packageName)
+                        putStringArray("ACTIVITY_LIST", arrayOf<String>("*"))
+                    }
+                )
+            )
+            putBundle("PLUGIN_CONFIG", Bundle().apply {
+                putString("PLUGIN_NAME", "INTENT")
+                putString("RESET_CONFIG", "false")
+                putBundle("PARAM_LIST", Bundle().apply {
+                    putString("intent_output_enabled", "true")
+                    putString("intent_action", SCAN_INTENT)
+                    putString("intent_category", "android.intent.category.DEFAULT")
+                    putInt("intent_delivery", USE_BROADCAST)
+                })
+            })
+        }
+
+        val i = Intent().apply {
+            setAction("com.symbol.datawedge.api.ACTION")
+            putExtra("com.symbol.datawedge.api.SET_CONFIG", config)
+        }
+        this.sendBroadcast(i)
     }
 }
 
@@ -106,7 +189,10 @@ data class InventoryItem(
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
-fun InventoryApp(modifier: Modifier = Modifier) {
+fun InventoryApp(
+    modifier: Modifier = Modifier,
+    barcodeBroadcastState: MutableState<String>? = null,
+) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
 
@@ -141,6 +227,15 @@ fun InventoryApp(modifier: Modifier = Modifier) {
             } catch (e: Exception) {
                 Log.e("InventoryApp", "Error getting data: ${e.message}")
                 errorMessage = "Error getting data: ${e.message}"
+            }
+        }
+    }
+
+    LaunchedEffect(barcodeBroadcastState?.value) {
+        barcodeBroadcastState?.value?.let { barcode ->
+            if (barcode.isNotEmpty()) {
+                inventoryNumber = barcode
+                search()
             }
         }
     }
