b9141ccdd3e781a4782ba4b3f4a7f71289b47ef9
robbi5 <maxi@richt.name>
2025-05-02 17:19:47 +0200

    Use yellow as primary color

--- app/src/main/java/de/temporaerhaus/inventory/client/ui/theme/Color.kt
+++ app/src/main/java/de/temporaerhaus/inventory/client/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Yellow80 = Color(0xFFFFE600)
+val YellowGrey80 = Color(0xFF7E7B60)
+val Yellow40 = Color(0xFFB3A300)
+val YellowGrey40 = Color(0xFF7E7B60)
