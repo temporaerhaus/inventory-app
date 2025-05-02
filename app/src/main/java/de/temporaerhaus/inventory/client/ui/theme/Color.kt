08517172c9a659099a4cb050af3e0909810d13c2
robbi5 <maxi@richt.name>
2025-05-02 18:00:44 +0200

    No dynamic color theme

--- app/src/main/java/de/temporaerhaus/inventory/client/ui/theme/Color.kt
+++ app/src/main/java/de/temporaerhaus/inventory/client/ui/theme/Color.kt
@@ -2,15 +2,10 @@ package de.temporaerhaus.inventory.client.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val Yellow80 = Color(0xFFFFE600)
 val YellowGrey80 = Color(0xFF7E7B60)
-val Yellow40 = Color(0xFFB3A300)
+val Yellow40 = Color(0xFFEFCA1B)
 val YellowGrey40 = Color(0xFF7E7B60)
