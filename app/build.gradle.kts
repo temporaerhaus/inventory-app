e48cbf0ff51b1c67507d92be4cf913eb6ee31195
robbi5 <maxi@richt.name>
2026-01-02 17:07:56 +0100

    Bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.temporaerhaus.inventory.client"
         minSdk = 23 // Android 6.0 (API 23) - so we can still use the torg devices
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         base.archivesName = "tph-inventory-client-${versionName}"
